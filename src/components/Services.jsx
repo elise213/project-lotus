569cacea37a3d3abf8521ab8e2797d5a00baa935
Nelson Chang <nelsonc924@gmail.com>
2023-08-21 09:46:06 -0400

    this is just a test

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -23,10 +23,7 @@ const Services = () => {
                     </p>
                 </div>
             </div>
-            <Modal 
-                idx={'123'}
-                image={'https://images.pexels.com/photos/274192/pexels-photo-274192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}
-            />
+            
             <div className='SEO-backed'>
                 <div className="service-text-cont">
                     <p className='service-title'>SEO Backed Web Design</p>
