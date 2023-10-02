8abb5d9515b40943d7404984513352e9e767837c
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-10-02 09:06:17 -0500

    lotus animation etc

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
-import Form from "./Form"
-import '../styles/contact.css'
-
+import Form from "./Form";
+import "../styles/contact.css";
 
 const Contact = () => {
-    const contactPage = useRef();
+  const contactPage = useRef();
 
-    return (
-        <div className="contact-page" ref={contactPage}>
-            <p className="getInTouch">Get in touch with CODELOTUS</p>
-            <Form />
-        </div>
-
-    );
+  return (
+    <div className="contact-page" ref={contactPage}>
+      <p className="getInTouch">Tell us about your project!</p>
+      <Form />
+    </div>
+  );
 };
-
 
 export default Contact;
